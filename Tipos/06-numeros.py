d83c1f9980d3c472018e53a25f6294f28da10bbb
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-05-09 15:02:33 -0400

    Create 06-numeros.py

---
+++ Tipos/06-numeros.py
@@ -0,0 +1,16 @@
+numero = 2  # entero -> integer
+decimal = 1.2  # float
+imaginario = 2 + 2j  # 2 +2i
+
+# numero = numero + 2
+numero += 2  # se le suma el numero indicado a la variable y se le asigna el nuevo valor
+
+print("numero es", numero)
+
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(1 / 3)
+print(1 // 3)
+print(8 % 3)  # el restante de una division
+print(2 ** 3)  # elevar a
