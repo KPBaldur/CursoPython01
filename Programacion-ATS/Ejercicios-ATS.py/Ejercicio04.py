ea713fa94654226649f715b1753458c3d8bc3d28
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-05-27 16:30:13 -0400

    Create Ejercicio04.py

---
+++ Programacion-ATS/Ejercicios-ATS.py/Ejercicio04.py
@@ -0,0 +1,11 @@
+import math
+
+pi_value = math.pi
+
+r = float(input("Ingresa el valor del radio: "))
+
+area = pi_value * r ** 2
+longitud = 2 * pi_value * r
+
+print(f"El valor del area del circulo es: {area}")
+print(f"Y el valor de su longitud es: {longitud}")
